13dc6ee25c9962b6db99f3172af36c8e0cf7aa2f
Geine's world <85022962+oidolee@users.noreply.github.com>
2024-02-29 16:30:31 +0900

    2024-02-29

---
+++ src/main/java/springBoot/ict/movie/SpringBootImMovieApplication.java
@@ -0,0 +1,13 @@
+package springBoot.ict.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootImMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootImMovieApplication.class, args);
+	}
+
+}
