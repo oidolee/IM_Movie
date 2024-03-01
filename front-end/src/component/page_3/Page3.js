6582f77b528ccb172056e49a7f5afea41a48efff
gyooooh <155943403+gyooooh@users.noreply.github.com>
2024-03-01 17:31:46 +0900

    commit

--- front-end/src/component/page_3/Page3.js
+++ front-end/src/component/page_3/Page3.js
@@ -1,9 +1,10 @@
 
 import React, {Component} from 'react';
 import storeBanner from '../../assets/page_3/storeBanner.jpg'
+import package1 from '../../assets/page_3/package1.jpg'
 import '../../styles/page_3/StoreList.css'
 
-class MainBody extends Component{
+class Page3 extends Component{
     render(){
         return(
             <div className='store'>
@@ -13,11 +14,36 @@ class MainBody extends Component{
 
                 <div className='contents'>
                     <div className='second_menu'>
-                        <li><a href='page_1'>베스트</a></li>
-                        <li><a href='page_2'>관람권</a></li>
-                        <li><a href='page_3'>스낵음료</a></li>
+                        <li style={{ width: '33%', left: '0' }}><a href='page_1' >베스트</a></li>
+                        <li style={{ width: '33%', left: '0' }}><a href='page_2' >관람권</a></li>
+                        <li style={{ width: '33%', left: '0' }}><a href='page_3' >스낵음료</a></li>
                     </div>
 
+                    <div className='store2'>
+                        <h3 className='store_title'>베스트</h3>
+
+                        <a href="/StoreDetail" className="store_item">
+                            <div className='store_img'>
+                                <img src={package1}></img>
+                            </div>
+                            <div className="item_content">
+                                <div className="badge_wrap"></div>
+                                <div className="item_title">
+                                <h4>[IM과 봄] 패키지</h4>
+                                <p>2D일반관람권 2매</p>
+                                </div>
+                                <div className="txt_price_wrap">
+                                    <span className="txt_sale">15%&nbsp;</span>
+                                    <span className="txt_price">22,000<em>원</em></span>
+                                    &nbsp;
+                                    <span className="txt_price_ins">26,000원</span>
+                                    &nbsp;
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    
                 </div>
             </div>
 
@@ -25,4 +51,4 @@ class MainBody extends Component{
     }
 }
 
-export default MainBody;
+export default Page3;
---
+++ front-end/src/component/page_3/StoreDetail.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import '../../styles/page_3/StoreDetail.css'
+import package1 from '../../assets/page_3/package1.jpg'
+
+class StoreDetail extends Component {
+    render() {
+        return (
+            <div className='store'>
+                <div className='contents'>
+                    <div className="main_img">
+                        <img src={package1} alt="[롯시와 봄] 패키지" />
+                    </div>
+
+                    <div className="pd_detail">
+                        <table className="pd_table" summary="상품 상세설명에 대한 표입니다">
+                            <caption>상품 상세내용</caption>
+                            <colgroup>
+                                <col style={{ width: '30%' }} />
+                                <col style={{ width: 'auto' }} />
+                            </colgroup>
+                            <thead>
+                                <tr>
+                                    <th scope="row" className="badge_wrap" colSpan="2"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row" className="tit" colSpan="2">[롯시와 봄] 패키지</th>
+                                </tr>
+                                <tr>
+                                    <ctd><span className="txt_sale">15%</span></td>
+                                    <td><span className="txt_price">22,000<em>원</em></span><span className="txt_price_ins">26,000원</span></td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">구성품</th>
+                                    <td>2D일반관람권 2매</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">구매제한</th>
+                                    <td>1인 1일 4매</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">유효기간</th>
+                                    <td>온라인 관람권 6 개월</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">사용가능 영화관</th>
+                                    <td>
+                                        <button id="availableCinema" className="btn_col4 ty3 rnd" title="레이어팝업 열기">온라인 관람권</button>&nbsp;
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div className="bx_num">
+                            <button className="btn_mins">삭제</button>
+                            <div className="txt_num">1</div>
+                            <button className="btn_plus">추가</button>
+                        </div>
+                        <div className="txt_price_wrap">
+                            총 상품금액<strong className="txt_price_str">22,000<em>원</em></strong>
+                        </div>
+                        <div className="btn_wrap">
+                            <button className="btn_col2 ty7">선물하기</button>
+                            <button className="btn_col1 ty7">구매하기</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default StoreDetail;
