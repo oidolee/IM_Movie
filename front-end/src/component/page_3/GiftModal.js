0149b1a8e33362d5eac005a2618662b3b10ed083
gyooooh <155943403+gyooooh@users.noreply.github.com>
2024-03-02 19:29:03 +0900

    commit

---
+++ front-end/src/component/page_3/GiftModal.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../../styles/page_3/StoreDetail.css'
+import package1 from '../../assets/page_3/package1.jpg'
+
+class StoreDetail extends Component {
+    render() {
+        return (
+            <div className='store_d'>
+                
+            </div>
+        );
+    }
+}
+
+export default StoreDetail;
