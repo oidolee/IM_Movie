8d58b8f2d781925a4ec3452cdf7a2b3fad7ef0bc
jjjjjinn <155942595+jjjjjinn@users.noreply.github.com>
2024-03-04 17:24:51 +0900

    예매페이지 수정

--- front-end/src/component/page_1/Reservation_Movie.js
+++ front-end/src/component/page_1/Reservation_Movie.js
@@ -1,177 +1,207 @@
-import React, {Component} from 'react';
-import moment from 'moment';
-import '../../styles/page_1/Reservation_Movie.css'
+import React, { Component } from "react";
+import moment from "moment";
+import "../../styles/page_1/Reservation_Movie.css";
+import Reservation_Swiper from "./Reservation_Swiper.js";
 
-class Reservation_Movie extends Component{
+class Reservation_Movie extends Component {
+  render() {
+    const sysdate = moment().format("YYYY-MM-DD");
+    const year = moment().format("YYYY");
+    const month = moment().format("MM");
+    const day = moment().format("DD");
 
-    render(){
-
-        return (
-            <div className="Res_menu">
-                <div id="Res_menu1">
-                    <ul>       
-                    <li className="step">
-                        <a href="#Res_step01">            
-                        <strong>
-                            <span>
-                            01<br />상영시간
-                            </span>
-                        </strong>
-                        <div className="step_content">
-                            <dl>
-                            <dt>선택한 영화 제목</dt>
-                            <dd></dd>
-                            <dt>선택한 상영관</dt>
-                            <dd></dd>
-                            <dt>선택한 상영 날짜</dt>
-                            <dd></dd>
-                            <dt>선택한 상영 시간</dt>
-                            <dd></dd>
-                            </dl>
-                        </div>
-                        </a>
-                    </li>               
-                    <li className="step">          
-                        <a href="#Res_step02">
-                        <strong>
-                            <span>
-                            02<br />인원/좌석
-                            </span>
-                        </strong>
-                        <div className="step_content">
-                            <dl>
-                            <dt>인원</dt>
-                            <dd></dd>
-                            <dt>좌석</dt>
-                            <dd></dd>
-                            </dl>
-                        </div>             
-                        </a>          
-                    </li>               
-                    <li className="step">     
-                        <a href="#Res_step03">             
-                        <strong>
-                            <span>
-                            03<br />결제
-                            </span>
-                        </strong>
-                        <div className="step_content">
-                            <dl>
-                            <dt>티켓금액</dt>
-                            <dd></dd>
-                            <dt>할인금액</dt>
-                            <dd></dd>
-                            <dt>총합계</dt>
-                            <dd></dd>
-                            </dl>
-                        </div>
-                        </a>
-                    </li>              
-                    <li className="step">
-                        <a href="#Res_step04">
-                        <strong>
-                            <span>
-                            04<br />결제완료
-                            </span>
-                        </strong>
-                        </a>
-                    </li>       
-                    </ul>
+    return (
+      <div className="Res_menu">
+        <div id="Res_menu1">
+          <ul>
+            <li className="step">
+              <a href="#Res_step01">
+                <strong>
+                  <span>
+                    01
+                    <br />
+                    상영시간
+                  </span>
+                </strong>
+                <div className="step_content">
+                  <dl>
+                    <dt>선택한 영화 제목</dt>
+                    <dd></dd>
+                    <dt>선택한 상영관</dt>
+                    <dd></dd>
+                    <dt>선택한 상영 날짜</dt>
+                    <dd></dd>
+                    <dt>선택한 상영 시간</dt>
+                    <dd></dd>
+                  </dl>
                 </div>
-                <div id="Res_menu2">
-                    <ul>         
-                    <li className="Res_tit">영화관</li>
-                    <li>
-                        <div className="menu2">
-                        <ul className="menu2_left">
-                            <li>전체</li>
-                            <li><a href="#none">서울</a></li>
-                            <li><a href="#none">경기/인천</a></li>
-                            <li><a href="#none">충청/대전</a></li>
-                            <li><a href="#none">전라/광주</a></li>
-                            <li><a href="#none">경북/대구</a></li>
-                            <li><a href="#none">경남/부산/울산</a></li>
-                            <li><a href="#none">강원</a></li>
-                            <li><a href="#none">제주</a></li>
-                        </ul>
-                        <ul className="menu2_right">
-                            <li><a href="#none">서울</a></li>
-                            <li><a href="#none">경기/인천</a></li>
-                            <li><a href="#none">충청/대전</a></li>
-                            <li><a href="#none">전라/광주</a></li>
-                            <li><a href="#none">경북/대구</a></li>
-                            <li><a href="#none">경남/부산/울산</a></li>
-                            <li><a href="#none">강원</a></li>
-                            <li><a href="#none">제주</a></li>
-                        </ul>
-                        </div>
-                    </li>
-                    </ul>     
+              </a>
+            </li>
+            <li className="step">
+              <a href="#Res_step02">
+                <strong>
+                  <span>
+                    02
+                    <br />
+                    인원/좌석
+                  </span>
+                </strong>
+                <div className="step_content">
+                  <dl>
+                    <dt>인원</dt>
+                    <dd></dd>
+                    <dt>좌석</dt>
+                    <dd></dd>
+                  </dl>
                 </div>
-                <div id="Res_menu3">
-                    <ul>         
-                    <li className="Res_tit">영화 선택</li>
-                    <li>
-                        <div className="menu2">
-                        <ul className="menu2_left">
-                            <li>전체</li>
-                            <li><a href="#none">서울</a></li>
-                            <li><a href="#none">경기/인천</a></li>
-                            <li><a href="#none">충청/대전</a></li>
-                            <li><a href="#none">전라/광주</a></li>
-                            <li><a href="#none">경북/대구</a></li>
-                            <li><a href="#none">경남/부산/울산</a></li>
-                            <li><a href="#none">강원</a></li>
-                            <li><a href="#none">제주</a></li>
-                        </ul>
-                        <ul className="menu2_right">
-                            <li><a href="#none">서울</a></li>
-                            <li><a href="#none">경기/인천</a></li>
-                            <li><a href="#none">충청/대전</a></li>
-                            <li><a href="#none">전라/광주</a></li>
-                            <li><a href="#none">경북/대구</a></li>
-                            <li><a href="#none">경남/부산/울산</a></li>
-                            <li><a href="#none">강원</a></li>
-                            <li><a href="#none">제주</a></li>
-                        </ul>
-                        </div>
-                    </li>
-                    </ul>     
+              </a>
+            </li>
+            <li className="step">
+              <a href="#Res_step03">
+                <strong>
+                  <span>
+                    03
+                    <br />
+                    결제
+                  </span>
+                </strong>
+                <div className="step_content">
+                  <dl>
+                    <dt>티켓금액</dt>
+                    <dd></dd>
+                    <dt>할인금액</dt>
+                    <dd></dd>
+                    <dt>총합계</dt>
+                    <dd></dd>
+                  </dl>
                 </div>
-                <div id="Res_menu4">
-                    <ul>         
-                    <li className="Res_tit">일정 선택</li>
-                    <li>
-                        <div className="menu2">
-                        <ul className="menu2_left">
-                            <li>전체</li>
-                            <li><a href="#none">서울</a></li>
-                            <li><a href="#none">경기/인천</a></li>
-                            <li><a href="#none">충청/대전</a></li>
-                            <li><a href="#none">전라/광주</a></li>
-                            <li><a href="#none">경북/대구</a></li>
-                            <li><a href="#none">경남/부산/울산</a></li>
-                            <li><a href="#none">강원</a></li>
-                            <li><a href="#none">제주</a></li>
-                        </ul>
-                        <ul className="menu2_right">
-                            <li><a href="#none">서울</a></li>
-                            <li><a href="#none">경기/인천</a></li>
-                            <li><a href="#none">충청/대전</a></li>
-                            <li><a href="#none">전라/광주</a></li>
-                            <li><a href="#none">경북/대구</a></li>
-                            <li><a href="#none">경남/부산/울산</a></li>
-                            <li><a href="#none">강원</a></li>
-                            <li><a href="#none">제주</a></li>
-                        </ul>
-                        </div>
-                    </li>
-                    </ul>     
-                </div>
-            </div>
-        );
-
-    }
+              </a>
+            </li>
+            <li className="step">
+              <a href="#Res_step04">
+                <strong>
+                  <span>
+                    04
+                    <br />
+                    결제완료
+                  </span>
+                </strong>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="Res_menu2">
+          <ul className="Res_left">
+            <li className="Res_tit">영화관</li>
+            <li>
+              <div className="menu2">
+                <ul className="menu2_left">
+                  <li>전체</li>
+                  <li>
+                    <a href="#none">서울</a>
+                  </li>
+                  <li>
+                    <a href="#none">경기/인천</a>
+                  </li>
+                  <li>
+                    <a href="#none">충청/대전</a>
+                  </li>
+                  <li>
+                    <a href="#none">전라/광주</a>
+                  </li>
+                  <li>
+                    <a href="#none">경북/대구</a>
+                  </li>
+                  <li>
+                    <a href="#none">경남/부산/울산</a>
+                  </li>
+                  <li>
+                    <a href="#none">강원</a>
+                  </li>
+                  <li>
+                    <a href="#none">제주</a>
+                  </li>
+                </ul>
+                <ul className="menu2_right">
+                  <li>
+                    <a href="#none">서울</a>
+                  </li>
+                  <li>
+                    <a href="#none">경기/인천</a>
+                  </li>
+                  <li>
+                    <a href="#none">충청/대전</a>
+                  </li>
+                  <li>
+                    <a href="#none">전라/광주</a>
+                  </li>
+                  <li>
+                    <a href="#none">경북/대구</a>
+                  </li>
+                  <li>
+                    <a href="#none">경남/부산/울산</a>
+                  </li>
+                  <li>
+                    <a href="#none">강원</a>
+                  </li>
+                  <li>
+                    <a href="#none">제주</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div id="Res_menu3">
+          <ul className="Res_left">
+            <li className="Res_tit">영화 선택</li>
+            <li>
+              <div className="menu3">
+                <ul className="menu3_left">
+                  <li>
+                    <a href="#none">서울</a>
+                  </li>
+                  <li>
+                    <a href="#none">경기/인천</a>
+                  </li>
+                  <li>
+                    <a href="#none">충청/대전</a>
+                  </li>
+                  <li>
+                    <a href="#none">전라/광주</a>
+                  </li>
+                  <li>
+                    <a href="#none">경북/대구</a>
+                  </li>
+                  <li>
+                    <a href="#none">경남/부산/울산</a>
+                  </li>
+                  <li>
+                    <a href="#none">강원</a>
+                  </li>
+                  <li>
+                    <a href="#none">제주</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div id="Res_menu4">
+          <ul className="Res_left">
+            <li className="Res_tit">{sysdate}</li>
+            <li>
+              <div className="menu4">
+                <ul className="menu4_right">
+                  <Reservation_Swiper />
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Reservation_Movie;
