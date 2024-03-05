bc9c44a855056e0ce9f204df09c2a2ba1d6e328f
Geine's world <85022962+oidolee@users.noreply.github.com>
2024-03-05 09:22:38 +0900

    예매 페이지 수정

--- front-end/src/component/main/Main_Swiper.js
+++ front-end/src/component/main/Main_Swiper.js
@@ -1,39 +1,24 @@
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import moment from "moment";
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-import style from  "../../styles/page_1/styles.css";
+import style from  "../../styles/main/Main_Swiper.module.css";
 
 // import required modules
 import { Navigation } from 'swiper/modules';
 
 export default function Reservation_Swiper() {
 
-  const sysdate = moment().format("YYYY-MM-DD");
-  const year = moment().format("YYYY");
-  const month = moment().format("MM");
-  const day = moment().format("D");
+
 
   return (
-    <div className={`Reservation_Swiper ${style.Reservation_Swiper}`}>
+    <>
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                                                                     
-        <SwiperSlide>
-          <div>
-            <ul>
-              <li>
-                {day}<br />
-                
-              </li>
-            </ul>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 22</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
@@ -42,6 +27,6 @@ export default function Reservation_Swiper() {
         <SwiperSlide>Slide 8</SwiperSlide>
         <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
-    </div>
+    </>
   );
 }
