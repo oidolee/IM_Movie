bd85afca3a27638ed26a986546c9ad17e6917398
jjjjjinn <155942595+jjjjjinn@users.noreply.github.com>
2024-03-05 18:24:38 +0900

    예매페이지 수정

--- IM_Movie.sql
+++ IM_Movie.sql
@@ -36,3 +36,131 @@ insert into mvc_sample_tbl
     values( nvl( (select max(id) from mvc_sample_tbl)+1 ,1), 'name_2', 'brand_2', 'madein_2', 2 );
 commit;    
 SELECT * FROM mvc_sample_tbl;
+
+
+----------------------------------------------------------------------------
+DROP TABLE mvc_sample_tbl CASCADE CONSTRAINTS;
+CREATE TABLE mvc_sample_tbl(
+   id      number(5)    PRIMARY KEY,
+   name    varchar2(50) NOT NULL,
+   brand   varchar2(50) NOT NULL,
+   madein  varchar2(50) NOT NULL,
+   price   number(9)    NOT NULL
+);
+insert into mvc_sample_tbl 
+    values( nvl( (select max(id) from mvc_sample_tbl)+1 ,1), 'name_1', 'brand_1', 'madein_1', 1 );
+insert into mvc_sample_tbl 
+    values( nvl( (select max(id) from mvc_sample_tbl)+1 ,1), 'name_2', 'brand_2', 'madein_2', 2 );
+commit;    
+SELECT * FROM mvc_sample_tbl;
+
+---------------------------------------------------------------
+DROP TABLE mvc_Reservation_tbl CASCADE CONSTRAINTS;
+CREATE TABLE mvc_Reservation_tbl(
+   Res_id           NUMBER          PRIMARY KEY,
+   c_email          VARCHAR2(50)    NOT NULL,
+   THEATER_ID       VARCHAR2(50)    NOT NULL,
+   SCREEN_ID        VARCHAR2(50)    NOT NULL,
+   MOV_AGE          VARCHAR2(255)   NOT NULL,
+   Res_movieDate    Date            DEFAULT sysdate,
+   Res_movieID      VARCHAR2(50)    NOT NULL,
+   Res_movieName    VARCHAR2(100)   NOT NULL,
+   Res_movieTime    VARCHAR2(100)   NOT NULL,
+   Res_seat         VARCHAR2(50)    NOT NULL,
+   Res_count        NUMBER          NOT NULL,
+   Res_ticketPrice  NUMBER          NOT NULL,
+   Res_sysdate      DATE            DEFAULT sysdate,
+   Res_check        CHAR(1)         DEFAULT 'n'
+);
+
+select * from mvc_Reservation_tbl;
+
+insert into mvc_Reservation_tbl(Res_id, c_email, THEATER_ID, SCREEN_ID, MOV_AGE, Res_movieDate, Res_movieID, Res_movieName, Res_movieTime, Res_seat, Res_count, Res_ticketPrice, Res_sysdate, Res_check)
+values ((SELECT NVL(MAX(Res_id)+1, 1) FROM mvc_Reservation_tbl), 'aa', 'aa', 'aa', 'aa', sysdate, 'aa', 'aa', 'aa', 'aa', 5, 5, sysdate, 'n');
+
+update mvc_Reservation_tbl
+set Res_check='y'
+where Res_id=1;
+
+delete from mvc_Reservation_tbl
+where Res_id=1;
+
+commit;
+
+DROP TABLE mvc_PayMent_tbl CASCADE CONSTRAINTS;
+CREATE TABLE mvc_PayMent_tbl(
+   Pay_id           NUMBER          PRIMARY KEY,
+   Res_id           NUMBER          NOT NULL,
+   c_email          VARCHAR2(50)    NOT NULL,
+   c_grade          VARCHAR2(50)    NOT NULL,
+   Pay_method       VARCHAR2(50)    NOT NULL,
+   Pay_payCompany   VARCHAR2(50)    NOT NULL,
+   Res_movieName    VARCHAR2(100)   NOT NULL,
+   Pay_cardNum      VARCHAR2(100)   NOT NULL,
+   Pay_tel          VARCHAR2(50)    NOT NULL,
+   Pay_sysdate      DATE            DEFAULT sysdate,
+   Pay_check        CHAR(1)         DEFAULT 'n'
+);
+
+select * from mvc_PayMent_tbl;
+
+insert into mvc_PayMent_tbl(Pay_id, Res_id, c_email, c_grade, Pay_method, Pay_payCompany, Res_movieName, Pay_cardNum, Pay_tel, Pay_sysdate, Pay_check)
+values ((SELECT NVL(MAX(Pay_id)+1, 1) FROM mvc_PayMent_tbl), 1, 'aa', 'aa', 'aa', 'aa', 'aa', 'aa', 'aa', sysdate, 'n');
+
+update mvc_PayMent_tbl
+set Pay_check='y'
+where Res_id=1;
+
+delete from mvc_PayMent_tbl
+where Res_id=1;
+
+commit;
+
+DROP TABLE mvc_Seat_tbl CASCADE CONSTRAINTS;
+CREATE TABLE mvc_Seat_tbl(
+   St_id            NUMBER          PRIMARY KEY,
+   THEATER_ID       VARCHAR2(50)    NOT NULL,
+   SCREEN_ID        VARCHAR2(50)    NOT NULL,
+   St_seatGroup     VARCHAR2(50)    NOT NULL,
+   St_num           VARCHAR2(50)    NOT NULL,
+   St_lineNum       VARCHAR2(50)    NOT NULL,
+   St_check         CHAR(1)         DEFAULT 'n'
+);
+
+select * from mvc_Seat_tbl;
+
+insert into mvc_Seat_tbl(St_id, THEATER_ID, SCREEN_ID, St_seatGroup, St_num, St_lineNum, St_check)
+values ((SELECT NVL(MAX(St_id)+1, 1) FROM mvc_Seat_tbl), 'aa', 'aa', 'aa', 'aa', 'aa', 'n');
+
+update mvc_Seat_tbl
+set St_check='y'
+where St_id=1;
+
+delete from mvc_Seat_tbl
+where St_id=1;
+
+commit;
+
+DROP TABLE mvc_Discount_tbl CASCADE CONSTRAINTS;
+CREATE TABLE mvc_Discount_tbl(
+   Dc_num            NUMBER          PRIMARY KEY,
+   Dc_mainTitle      VARCHAR2(50)    NOT NULL,
+   Dc_subTitle       VARCHAR2(50),
+   Dc_mainImg        VARCHAR2(255)   NOT NULL,
+   Dc_subImg         VARCHAR2(255)   NOT NULL,
+   Dc_show           CHAR(1)         DEFAULT 'n'
+);
+
+select * from mvc_Discount_tbl;
+
+insert into mvc_Discount_tbl(Dc_num, Dc_mainTitle, Dc_subTitle, Dc_mainImg, Dc_subImg, Dc_show)
+values ((SELECT NVL(MAX(Dc_num)+1, 1) FROM mvc_Discount_tbl), 'aa', 'aa', 'aa', 'aa', 'n');
+
+update mvc_Discount_tbl
+set Dc_show='y'
+where Dc_num=1;
+
+delete from mvc_Discount_tbl
+where Dc_num=1;
+
+commit;
--- front-end/src/component/page_1/Reservation_Swiper.js
+++ front-end/src/component/page_1/Reservation_Swiper.js
@@ -1,46 +1,67 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React from 'react';
 import moment from "moment";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
 
-// Import Swiper styles
+import 'moment/locale/ko';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
-
-import style from  "../../styles/page_1/styles.css";
-
-// import required modules
-import { Navigation } from 'swiper/modules';
+import style from  "../../styles/page_1/Reservation_Swiper.css";
 
 export default function Reservation_Swiper() {
 
   const sysdate = moment().format("YYYY-MM-DD");
-  const year = moment().format("YYYY");
-  const month = moment().format("MM");
-  const day = moment().format("D");
+
+  // 날짜 목록 생성(5일)
+  const getDates = (start) => {
+    const dates = [];
+    for (let i = 0; i < 5; i++) {
+      const date = moment(start).add(i, 'days').format("YYYY-MM-DD");
+      dates.push(date);
+    }
+    return dates;
+  }
+
+  // sysdate 달의 시작
+  const getSlideStartDate = (slideIndex) => {
+    return moment(sysdate).add(slideIndex * 5, 'days');
+  }
+
+  // 다음 달의 시작
+  const getNextMonthStartDate = (slideIndex) => {
+    const currentMonthEndDate = moment(sysdate).endOf('month');
+    return moment(currentMonthEndDate).add(1, 'day');
+  }
 
   return (
     <div className={`Reservation_Swiper ${style.Reservation_Swiper}`}>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                                                                     
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper"> 
+        {[0, 1, 2, 3, 4].map((slideIndex) => (
+          <SwiperSlide key={slideIndex}>
+            <div>
+              <ul className="swiper_content">
+                {getDates(getSlideStartDate(slideIndex)).map((date, index) => (
+                  <li key={index} style={{color: moment(date).day() === 0 ? 'red' : 'black'}}>
+                    <a href="">{moment(date).format("D")}<br />{moment(date).format("dd")}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </SwiperSlide>
+        ))}
+        {/* 다음 달로 이동하는 슬라이드 추가 */}
         <SwiperSlide>
           <div>
-            <ul>
-              <li>
-                {day}<br />
-                
-              </li>
+            <ul className="swiper_content">
+              {getDates(getNextMonthStartDate()).map((date, index) => (
+                <li key={index} style={{color: moment(date).day() === 0 ? 'red' : 'black'}}>
+                  <a href="">{moment(date).format("D")}<br />{moment(date).format("dd")}</a>
+                </li>
+              ))}
             </ul>
           </div>
         </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
     </div>
   );
